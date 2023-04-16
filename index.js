f305ac9831159cf33e81fe2b973bd7d4b52f7a0e
cedric <cedricj765@gmail.com>
2023-04-16 15:07:09 -0500

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+const domChild = document.querySelector('#main')
+domChild.remove()
+
+const newHeader = document.createElement('h1')
+
+newHeader.id = 'victory'
+newHeader.textContent = 'Cedric Jones is the champion'
+document.body.append(newHeader)
